8c3033c8c2ec8863e4d6017093053c0326d540c3
Stuart Berg <bergs@janelia.hhmi.org>
2019-04-25 10:44:10 -0400

    draco: Support debug builds (not sure if this works)

--- draco/build.sh
+++ draco/build.sh
@@ -9,9 +9,14 @@ if [[ $(uname) == "Darwin" ]]; then
     CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -stdlib=libc++ -fPIC -g"
 fi
 
+if [[ "${CMAKE_BUILD_TYPE}" == "" ]]; then
+    CMAKE_BUILD_TYPE=Release
+fi
+
 cmake .. \
     -Wno-dev \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+    -DBUILD_SHARED_LIBS="ON" \
     -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
     -DCMAKE_PREFIX_PATH:PATH="${PREFIX}" \
