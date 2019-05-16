feef9694073b6d40640709e99af303a58139f8d2
Stuart Berg <bergs@janelia.hhmi.org>
2019-05-16 09:33:55 -0400

    go: Remove 'malformed' test data to avoid choking on those files

--- recipes/go/build.sh
+++ recipes/go/build.sh
@@ -1,13 +1,25 @@
+#!/bin/bash
+
+# Simply copy the entire tarball contents into the PREFIX
 mkdir -p ${PREFIX}/go
 cp -r * ${PREFIX}/go
-mkdir -p ${PREFIX}/bin
 
+# The vendored pprof package includes a test suite that
+# contains intentionally "malformed" Mach-O and ELF binaries.
+#
+# Unfortunately, conda-build discovers those files and tries to analyze them, which crashes.
+# We don't need to run the pprof test suite, so just remove those "malformed" files.
+rm ${PREFIX}/go/src/cmd/vendor/github.com/google/pprof/internal/binutils/testdata/malformed_*
+
+# Symlink everything from go/bin directly into bin
+mkdir -p ${PREFIX}/bin
 cd ${PREFIX}/bin
 for f in ${PREFIX}/go/bin/*; do
-ln -s $f
+  ln -s $f
 done
 cd -
 
+# Install activation script (sets GOROOT).
 mkdir -p ${PREFIX}/etc/conda/activate.d
 cp ${RECIPE_DIR}/activate-go.sh ${PREFIX}/etc/conda/activate.d
 mkdir -p ${PREFIX}/etc/conda/deactivate.d
